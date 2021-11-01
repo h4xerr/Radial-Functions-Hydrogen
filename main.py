64cd4d891d37d8e2275d80d244b3fe69e2665af0
h4xerr <78847517+h4xerr@users.noreply.github.com>
2021-11-02 00:19:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ res = [0]*30
 for i in range(30):
     r=i/1
     res[i]=(z/a)**(3/2)*(2*math.exp((-z*r)/a))                                                                #R10
-    res[i]=(z/(2*a))**(3/2)*(2*(1-(z*r/(2*a))))*math.exp((-z*r)/(2*a))                                        #R20
-    res[i]=(z/(2*a))**(3/2)*(2/math.sqrt(3))*(z*r/2*a)*math.exp((-z*r)/2*a)                                   #R21
-    res[i]=(z/(3*a))**(3/2)*2*(1-2*z*r/(3*a)+2/3*(z*r/(3*a))**2)*math.exp((-z*r)/(3*a))                       #R30
-    res[i]=(z/(3*a))**(3/2)*(4*math.sqrt(2)/3)*((z*r)/(3*a))*(1-(1/2)*((z*r)/(3*a)))*math.exp((-z*r)/(3*a))   #R31
-    res[i]=(z/(3*a))**(3/2)*(2*math.sqrt(2)/(3*math.sqrt(5)))*(((z*r)/(3*a))**2)*math.exp((-z*r)/(3*a))        #R32
+    #res[i]=(z/(2*a))**(3/2)*(2*(1-(z*r/(2*a))))*math.exp((-z*r)/(2*a))                                        #R20
+    #res[i]=(z/(2*a))**(3/2)*(2/math.sqrt(3))*(z*r/2*a)*math.exp((-z*r)/2*a)                                   #R21
+    #res[i]=(z/(3*a))**(3/2)*2*(1-2*z*r/(3*a)+2/3*(z*r/(3*a))**2)*math.exp((-z*r)/(3*a))                       #R30
+    #res[i]=(z/(3*a))**(3/2)*(4*math.sqrt(2)/3)*((z*r)/(3*a))*(1-(1/2)*((z*r)/(3*a)))*math.exp((-z*r)/(3*a))   #R31
+    #res[i]=(z/(3*a))**(3/2)*(2*math.sqrt(2)/(3*math.sqrt(5)))*(((z*r)/(3*a))**2)*math.exp((-z*r)/(3*a))        #R32
     print(res[i])
 plt.plot(res,label='result')
 plt.show()
